87ef58dceeccf02052e0b9cff8e76fad4246682d
mayankkwl <mayank.khandelwal@gmail.com>
2015-04-01 12:27:22 -0400

    Insert code added.

--- TheServerMan/TheServerMan.js
+++ TheServerMan/TheServerMan.js
@@ -17,8 +17,18 @@ if (Meteor.isClient) {
 		"click .close" : function(){
 			$(".modal").hide();
 		},
-		
+		"click .submit" : function(evt, tmpl){
+			var name = tmpl.find(".name").value;
+			var size = tmpl.find(".size").value;
+			var link = tmpl.find(".link").value;
+			var version = tmpl.find(".version").value;
+			addNewAppForm(name, size, link, version);
+			$(".modal").hide();
+		},
 	});	
+	var addNewAppForm = function(name, size, link, version){
+		_Applications.insert({name:name, size: size, link: link, version:version});
+	}
 }
 
 if (Meteor.isServer) {
